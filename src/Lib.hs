fcdce71a47f629b8a835f5065f29b5b4feb78866
Akash <ashakdwipeea@gmail.com>
2018-08-01 01:42:47 +0530

    add modelling via TChans

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Concurrent.STM.TChan
 
 data Suit = Club
           | Diamond
@@ -9,32 +14,19 @@ data Suit = Club
           | Spade
           deriving (Eq, Ord, Show, Enum)
 
-data Rank = Jack | Queen | King | Ace | Two | Three | Four | Five
-          | Six | Seven | Eight | Nine | Ten
-              deriving (Eq, Ord, Show, Enum)
+data Rank = Jack | Nine | Ace | Ten | King | Queen  | Eight | Seven
+          | Two | Three | Four | Five | Six
+          deriving (Eq, Ord, Show, Enum)
 
-
--- newtype NumberCard = UnsafeMkNumberCard { n :: Int }
-
--- mkNumberCard :: Int -> Maybe NumberCard
--- mkNumberCard n
---   | n `elem` [2 .. 10] = Just (UnsafeMkNumberCard n)
---   | otherwise = Nothing
-
-
--- data Rank = Rank FaceCard | NumberCard
---           deriving (Eq, Ord, Show)
-
-newtype Points = UnsafeMkPoints { p :: Int }
+newtype Points = Points { p :: Int }
                deriving (Eq, Ord, Show)
 
 mkPoints :: Rank -> Points
-mkPoints Jack = UnsafeMkPoints 3
-mkPoints Nine = UnsafeMkPoints 2
-mkPoints Ace = UnsafeMkPoints 1
-mkPoints Ten = UnsafeMkPoints 1
-mkPoints _ = UnsafeMkPoints 0
-
+mkPoints Jack = Points 3
+mkPoints Nine = Points 2
+mkPoints Ace = Points 1
+mkPoints Ten = Points 1
+mkPoints _ = Points 0
 
 data Card = Card {suit :: Suit,
                   rank :: Rank,
@@ -46,5 +38,75 @@ type Deck = [Card]
 genDeck :: Deck
 genDeck = [Card suit rank (mkPoints rank) | suit <- [Club .. Spade], rank <- [Jack .. Ten]]
 
+isPlayableRank :: Rank -> Bool
+isPlayableRank rank
+  | rank `elem` [Jack , Nine , Ace , Ten , King , Queen  , Eight , Seven] = True
+  | otherwise = False
+
+getPlayableCards :: Deck
+getPlayableCards = filter (isPlayableRank . rank)  genDeck
+
+
+data RedealReply = Redeal | Continue
+                 deriving (Eq, Show)
+
+data PlayerMessage = MRedeal RedealReply
+             deriving (Eq, Show)
+
+data RedealMsg = WantRedeal deriving (Eq, Show)
+
+data DealerMessage = MRedealQ RedealMsg
+                     deriving (Eq, Show)
+
+data Player = Player {name :: String
+                     ,score :: Int
+                     ,hand :: Deck
+                     ,writeChan :: TChan PlayerMessage
+                     ,readChan :: TChan DealerMessage}
+            deriving (Eq, Show)
+
+instance Show (TChan c)  where
+  show c = "A channel " ++ show c
+
+data Game = Game {players :: [Player]
+                 ,deck :: Deck}
+          deriving (Eq, Show)
+
+type Name = String
+
+mkPlayer :: Name -> IO Player
+mkPlayer n = do
+  writeChan <- atomically $ newTChan
+  readChan <- atomically $ newTChan
+  return Player {name = n
+                ,score = 0
+                ,hand = []
+                ,writeChan = writeChan
+                ,readChan = readChan}
+
+genTestPlayers :: IO [Player]
+genTestPlayers = mapM (\i -> mkPlayer $ "player-" ++ show i) [1 .. 4]
+
+mkGame :: IO Game
+mkGame = fmap (\p -> Game {players = p, deck = getPlayableCards})  genTestPlayers
+
+distribute :: Player -> Card -> Player
+distribute p@(Player {hand}) c = p { hand = c : hand}
+
+dealCards :: Game -> Game
+dealCards (Game {players, deck}) = let (currentDeal, restDeck) = splitAt (length players) deck
+                                 in  Game { players = zipWith distribute players currentDeal
+                                          , deck = restDeck}
+
+
+isRedealPossible :: Game -> Bool
+isRedealPossible (Game {players, deck}) = any hasPoints $ (hand . head) players
+  where
+    hasPoints (Card {points}) = (p points) > 0
+
+
+-- startGame :: Game
+-- startGame = let game = dealCards mkGame in
+  
 someFunc :: IO ()
 someFunc = putStrLn "Hello there!!"
